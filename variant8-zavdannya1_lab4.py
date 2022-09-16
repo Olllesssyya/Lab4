f9d413077fd1fde7c1cd430d228671bd242b67cf
Olesia Fesiuk <O.Fesyuk_FIT_9_22_B_d@knute.edu.ua>
2022-09-16 15:14:46 +0300

    Перша редакція.txt

---
+++ variant8-zavdannya1_lab4.py
@@ -0,0 +1,9 @@
+import math
+x=float(input("Введіть х:"))
+#z=math.log(math.cos(math.radians(x)))
+print ("cos=",math.cos(x))
+#z=math.log10(math.cos(x))
+#обчислюємо нижню частину
+#w=2-(math.pow((x**2+1), 1/3)) # x**2 - возведення у степень, 1/3 - кубічний корень
+#y=math.pow(4,(2*x))-z/w
+#print("Значення функції:",y)
